69fe2431a3802fd84ce2d72d5634215178cea518
Naqoosh Fatima <naqoosh.fatima@gmail.com>
2026-02-03 16:20:57 +0530

    summary field updation issue fixed

--- src/components/saveDraftDialog.vue
+++ src/components/saveDraftDialog.vue
@@ -287,7 +287,11 @@ export default {
       //   this.form.summary = "";
       //   return;
       // }
-
+      this.$nextTick(() => {
+      if (
+        !this.finalPlanData?.originalData ||
+        !this.finalPlanData?.currentData
+      ) return;
       const diff = this.diffNodes(
         this.finalPlanData.originalData,
         this.finalPlanData.currentData,
@@ -295,6 +299,7 @@ export default {
       );
 
       this.form.summary = this.buildSummaryText(diff);
+      })
     },
     isEdit(val) {
       console.log("val===============", val);
